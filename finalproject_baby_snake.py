c0ce47b39ef799602fb94cea791d41fc8fe65a95
flora8heart <9201125+flora8heart@users.noreply.github.com>
2025-03-03 18:59:36 +0000

    Set up world

---
+++ finalproject_baby_snake.py
@@ -0,0 +1,35 @@
+from graphics import Canvas
+import time
+import random
+    
+CANVAS_WIDTH = 400
+CANVAS_HEIGHT = 400
+SIZE = 20
+
+# if you make this larger, the game will go slower
+DELAY = 0.1 
+
+def main():
+    canvas = Canvas(CANVAS_WIDTH, CANVAS_HEIGHT)
+    
+    # TODO: your code here
+    # Player square
+    player = canvas.create_rectangle(
+        0,
+        0,
+        SIZE,
+        SIZE,
+        "blue"
+    )
+
+    goal = canvas.create_rectangle(
+        360,
+        360,
+        360 + SIZE,
+        360 + SIZE,
+        "salmon"
+    )
+        
+
+if __name__ == '__main__':
+    main()
