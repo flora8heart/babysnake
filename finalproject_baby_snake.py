eeca4ce56cd1577d00265cfe3c4e0f25954c2321
flora8heart <9201125+flora8heart@users.noreply.github.com>
2025-03-06 11:02:35 +0000

    Add key press

--- finalproject_baby_snake.py
+++ finalproject_baby_snake.py
@@ -25,8 +25,8 @@ def main():
     goal = canvas.create_rectangle(
         360,
         360,
-        360 + SIZE,
-        360 + SIZE,
+        360+SIZE,
+        360+SIZE,
         "salmon"
     )
 
@@ -35,17 +35,43 @@ def main():
         10,
         380,
         anchor='w',
+        font_size = 20,
         text=f"score: {score}"
     )
-
+        
     # Animation loop - player
+    cur_direction = "right"
     while True:
         # update the world
-        canvas.move(player, SIZE, 0)
+        print(f"current direction: {cur_direction}")
+        if cur_direction == "right":
+            canvas.move(player, SIZE, 0) 
+        elif cur_direction == "left":
+            canvas.move(player, - SIZE, 0) 
+        elif cur_direction == "up":
+            canvas.move(player, 0, - SIZE)
+        elif cur_direction == "down":
+            canvas.move(player, 0, SIZE)
+
+
+        # Set up key press
+        key = canvas.get_last_key_press()
+
+        if key == "ArrowLeft":
+            print("left arrow pressed!")
+            cur_direction = "left"
+        elif key == "ArrowRight":
+            print("righ arrow pressed!")
+            cur_direction = "right"
+        elif key == "ArrowUp":
+            print("up arrow pressed!")
+            cur_direction = "up"
+        elif key == "ArrowDown":
+            print("down arrow pressed!")
+            cur_direction = "down"
 
         # sleep
         time.sleep(DELAY)
-        
 
 if __name__ == '__main__':
     main()
